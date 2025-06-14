b942890c5c132aea4e5c0bc4b30b4c41c9969562
Vlad Glivuk <vlad.glivuk@gmail.com>
2025-06-14 17:57:55 +0300

    update hardchat config

--- back/hardhat.config.cjs
+++ back/hardhat.config.cjs
@@ -1,13 +1,20 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
+    optimismSepolia: {
       url: process.env.INFURA_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  }
 };
